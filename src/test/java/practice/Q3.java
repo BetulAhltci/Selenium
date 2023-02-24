a730b11dc2e5beab449e803235bb66c80fdd34a8
BetulAhlatci <betulahlatci13@gmail.com>
2023-02-24 21:47:19 +0300

    24.02.2023

--- src/test/java/practice/Q3.java
+++ src/test/java/practice/Q3.java
@@ -1,8 +1,25 @@
 package practice;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class Q3 {
+    @BeforeClass
+    public static void sethup(){
+        System.out.println("BeforeClass");
+    }
+    @AfterClass
+    public static void teardown(){
+        System.out.println("AfterCilass");
+    }
+    @Before
+    public void before(){
+        System.out.println("Before");
+    }
+    @After
+    public void after(){
+        System.out.println("After");
+    }
+
     @Test
     public void ahmet(){
         System.out.println("Ahmet");
